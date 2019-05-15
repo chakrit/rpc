d1a9b0ce129f95734badf579208aeacceb5cd223
Chakrit Wichian <chakrit@omise.co>
2019-05-15 13:54:41 +0700

    [elm] Update test case to include new time support

--- cases/100_generators/elm/Rpc/Todo.elm
+++ cases/100_generators/elm/Rpc/Todo.elm
@@ -16,14 +16,14 @@ type alias User =
 encodeUser : User -> E.Value
 encodeUser obj =
     E.object
-        [ ( "ctime", (Time.posixToMillis >> E.int) obj.ctime )
+        [ ( "ctime", (Time.posixToMillis >> toFloat >> (\f -> f/1000.0) >> E.float) obj.ctime )
         , ( "username", E.string obj.username )
         ]
 
 decodeUser : D.Decoder User
 decodeUser =
     D.map2 User
-            (D.field "ctime" ((D.map Time.millisToPosix D.int)))
+            (D.field "ctime" ((D.map ((\f -> f * 1000.0) >> round >> Time.millisToPosix) D.float)))
             (D.field "username" (D.string))
     
 
